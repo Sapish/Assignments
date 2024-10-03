d964aa363979b7d38d1ffae3c0bbc267155ea425
sapish <oskardb@uia.no>
2024-10-04 00:53:39 +0200

    added more settings to norwegian and english

--- portfolio2/Game/language.mjs
+++ portfolio2/Game/language.mjs
@@ -3,10 +3,20 @@ const DICTIONARY = {
     en: {
         PLAY_AGAIN_QUESTION: "Play again (YES/no)? ",
         CONFIRM: "y",
+        MENU_CHOICE_START_GAME: "Start Game",
+        MENU_CHOICE_SHOW_SETTINGS: "Settings",
+        MENU_CHOICE_EXIT_GAME: "Exit Game",
+        SETTINGS_MENU_CHOICE_LANGUAGE: "Chose Language",
+        SETTINGS_MENU_CHOICE_BACK: "Back to Main Menu",
     },
     no: {
         PLAY_AGAIN_QUESTION: "Spille en gang til (Ja/nei)? ",
         CONFIRM: "j",
+        MENU_CHOICE_START_GAME: "Start Spill",
+        MENU_CHOICE_SHOW_SETTINGS: "Innstillinger",
+        MENU_CHOICE_EXIT_GAME: "Avslutt Spill",
+        SETTINGS_MENU_CHOICE_LANGUAGE: "Språk alternativer",
+        SETTINGS_MENU_CHOICE_BACK: "Tilbake til hovedmeny",
     }
 }
 
