39101f99b23c3c55fd133f4f9976d27f18c83d64
sapish <oskardb@uia.no>
2024-11-28 18:59:05 +0100

    function to initialize the application

--- portfolio4/game (3).mjs
+++ portfolio4/game (3).mjs
@@ -1,4 +1,5 @@
 import Labyrinth from "./labyrint.mjs"
+import SplashScreen from "./splashScreen.mjs";
 import ANSI from "./utils/ANSI.mjs";
 
 const REFRESH_RATE = 250;
@@ -10,19 +11,19 @@ let isBlocked = false;
 let state = null;
 
 function init() {
-    //All levels available to the game. 
-    state = new Labyrinth();
-    intervalID = setInterval(update, REFRESH_RATE);
+    const splash = new SplashScreen();
+    splash.animate(() => {
+        state = new Labyrinth();
+        intervalID = setInterval(update, REFRESH_RATE);    
+    });
 }
 
 function update() {
 
     if (isBlocked) { return; }
     isBlocked = true;
-    //#region core game loop
     state.update();
     state.draw();
-    //#endregion
     isBlocked = false;
 }
 
